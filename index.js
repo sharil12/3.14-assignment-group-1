a85bc121bab6c28fb756ff3a6b95652ba04e1c70
chongcaroline <chong.caroline@gmail.com>
2023-11-22 20:24:46 +0800

    2 file commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-test
+module.exports.handler = async (event) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: "Welcome to Group 1",
+        },
+        null,
+        2
+      ),
+    };
+  };
